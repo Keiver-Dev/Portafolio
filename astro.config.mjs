9b4eb66c47078404ad898a3218f9aac0372d7f87
Keiver-Dev <keiverlunaps11@gmail.com>
2025-04-06 20:45:13 -0300

    Config Tailwind V 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://Portafolio.github.io',
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  site: 'https://Keiver-Dev.github.io',
+  base: '/',
+  integrations: [react(), tailwind()],
 });
