5987bcf7ee82f32b0733dfc5cc1653dd0a3ec88a
Keiver-Dev <keiverlunaps11@gmail.com>
2025-04-06 21:04:30 -0300

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://keiver-dev.github.io',
-  base: '/Portafolio/',
+  base: '/Portafolio/', // Asegúrate de que coincida con el nombre de tu repositorio
   integrations: [react(), tailwind()],
 });
